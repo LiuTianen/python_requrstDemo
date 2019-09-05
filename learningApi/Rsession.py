74ea05638856f9a161a6c3e5cf5aab22d034ad6c
Tinen <liuyongyi@kobox.tv>
2019-09-05 17:58:28 +0800

    调整打印信息

--- learningApi/Rsession.py
+++ learningApi/Rsession.py
@@ -2,9 +2,9 @@ import requests
 
 s = requests.Session()
 r = s.post("http://127.0.0.1:5000/user_login",data={"username":"jack", "password":"123"})
-
+print(r.json())
 print(r.cookies)
 
 r2 = s.get("http://127.0.0.1:5000/user_data")
-
+print(r2.json())
 print(r2.cookies)
